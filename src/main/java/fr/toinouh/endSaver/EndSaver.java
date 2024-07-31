bd4d4cad910c6092efadbbe4f37147f92da3b6e5
toinouH <toinouh2003@gmail.com>
2024-07-31 15:57:30 +0200

    Small clean

--- src/main/java/fr/toinouh/endSaver/EndSaver.java
+++ src/main/java/fr/toinouh/endSaver/EndSaver.java
@@ -8,13 +8,10 @@ public final class EndSaver extends JavaPlugin {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new PlayerVoidListener(this), this);
         getLogger().info("EndVoidTeleportPlugin has been enabled.");
-        // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
         getLogger().info("EndVoidTeleportPlugin has been disabled.");
     }
 }
